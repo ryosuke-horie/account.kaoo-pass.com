6a563eadd23511cbe6fa8b4aedcbd87fe7a2a7c4
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-16 10:11:56 +0900

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,5 +8,10 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'modul'
   }
 }
