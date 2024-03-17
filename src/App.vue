4602605bca754b816742306cce75b4da82574e3f
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-17 13:46:58 +0900

    Login用コンポーネントを用意

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import AdminLayout from './layouts/AdminLayout.vue';
+import Login from './components/Login.vue';
 </script>
 
 <template>
     <AdminLayout>
         この中でコンテンツを表示します
+        <Login />
     </AdminLayout>
 </template>
 
