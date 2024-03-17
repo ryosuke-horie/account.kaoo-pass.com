68648fc0472aea50eebf9739c1bf72bf0e9f038e
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-17 13:04:57 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    'node_modules/preline/dist/*.js',
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {},
