0f55ea4b4de4415f4c0205b9de9f99b964da2897
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-17 13:37:30 +0900

    Vuetifyでデザイン用意

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import AdminLayout from './layouts/AdminLayout.vue';
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      >
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      >
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <AdminLayout>
+        この中でコンテンツを表示します
+    </AdminLayout>
 </template>
 
 <style scoped>
---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="｛ジム名｝ 会員管理"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="ナビゲーションメニューを記載する場所"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <slot></slot>
+    </v-main>
+  </v-layout>
+</template>
