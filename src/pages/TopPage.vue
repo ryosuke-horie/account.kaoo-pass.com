429bbec66338151ea50a964a18a6cb96224c4def
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-03-25 12:41:23 +0900

    lint

--- src/pages/TopPage.vue
+++ src/pages/TopPage.vue
@@ -1,3 +1,3 @@
 <template>
-    Top
+  Top
 </template>
--- src/pages/UserDashBoardPage.vue
+++ src/pages/UserDashBoardPage.vue
@@ -41,15 +41,19 @@ const headers = ref([
 </script>
 
 <template>
-    <v-data-table-virtual :headers="headers" :items="users" item-value="name">
-        <template v-slot:item="{ item }">
-            <tr>
-                <td>{{ item.name }}</td>
-                <!-- <td>{{ item.age }}</td> -->
-                <!-- <td>{{ item.address }}</td> -->
-                <td>{{ item.email }}</td>
-                <!-- <td>{{ item.phone }}</td> -->
-            </tr>
-        </template>
-    </v-data-table-virtual>
+  <v-data-table-virtual
+    :headers="headers"
+    :items="users"
+    item-value="name"
+  >
+    <template #item="{ item }">
+      <tr>
+        <td>{{ item.name }}</td>
+        <!-- <td>{{ item.age }}</td> -->
+        <!-- <td>{{ item.address }}</td> -->
+        <td>{{ item.email }}</td>
+        <!-- <td>{{ item.phone }}</td> -->
+      </tr>
+    </template>
+  </v-data-table-virtual>
 </template>
