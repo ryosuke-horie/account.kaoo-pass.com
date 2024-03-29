69ed95215ec6eed5e5c91fbd4d0549c32c783748
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-03-29 20:19:10 +0900

    必須項目の用意

--- src/pages/UserAddPage.vue
+++ src/pages/UserAddPage.vue
@@ -3,17 +3,23 @@ import { ref } from 'vue'
 import TextInput from '../components/Form/TextInput.vue'
 import FileInput from '../components/Form/FileInput.vue'
 
-// 氏名
-const name = ref('') // 氏名
-const nameRule = [
+// 必須項目チェックのルール
+const requreRule = [
   value => {
     if (value) return true
     return '必須項目です。'
   },
 ]
 
+const name = ref('') // 氏名
+const mail = ref('') // メールアドレス
+const phone = ref('') // 電話番号
+
+
 // 画像
 const avatarImage = ref(null)
+const image2 = ref(null)
+const image3 = ref(null)
 </script>
 
 <template>
@@ -24,13 +30,34 @@ const avatarImage = ref(null)
     <v-form @submit.prevent>
       <TextInput
         v-model="name"
-        :rules="nameRule"
+        :rules="requreRule"
         label="氏名"
       />
+        <TextInput
+            v-model="mail"
+            :rules="requreRule"
+            label="メールアドレス"
+        />
+        <TextInput
+            v-model="phone"
+            :rules="requreRule"
+            label="電話番号"
+        />
+
       <FileInput
         v-model="avatarImage"
         accept="image/*"
         label="顔写真1"
+      />
+      <FileInput
+        v-model="image2"
+        accept="image/*"
+        label="顔写真2"
+      />
+      <FileInput
+        v-model="image3"
+        accept="image/*"
+        label="顔写真3"
       />
       <v-btn
         class="mt-2"
