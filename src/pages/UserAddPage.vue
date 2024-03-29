11bf3b86ccba169b2b1907f61d545650b5cd57e6
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-03-29 20:40:02 +0900

    送信処理の実装

--- src/pages/UserAddPage.vue
+++ src/pages/UserAddPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import TextInput from '../components/Form/TextInput.vue'
 import FileInput from '../components/Form/FileInput.vue'
 
@@ -20,6 +20,46 @@ const phone = ref('') // 電話番号
 const avatarImage = ref(null)
 const image2 = ref(null)
 const image3 = ref(null)
+
+// フォームの値を登録するsubmitアクション
+const submitForm = async () => {
+  // フォームの値をオブジェクトにまとめる
+  const formData = {
+    name: name.value,
+    mail: mail.value,
+    phone: phone.value,
+    avatarImage: avatarImage.value,
+    image2: image2.value,
+    image3: image3.value,
+  }
+
+  // 空の値がある場合はアラートを表示して処理を中断する
+    if (!formData.name || !formData.mail || !formData.phone || !formData.avatarImage || !formData.image2 || !formData.image3) {
+        alert('すべての項目を入力してください。')
+        return
+    }
+
+  try {
+    // サーバーにデータを送信する処理を実装する
+    // 例えば、axios を使用して API リクエストを送信する
+    // const response = await axios.post('/api/register', formData)
+
+    // 送信が成功した場合はアラートを表示する
+    alert('会員登録が完了しました。')
+
+    // フォームの値をリセットする
+    name.value = ''
+    mail.value = ''
+    phone.value = ''
+    avatarImage.value = null
+    image2.value = null
+    image3.value = null
+  } catch (error) {
+    // エラーハンドリングを行う
+    console.error(error)
+    alert('会員登録に失敗しました。')
+  }
+}
 </script>
 
 <template>
@@ -34,7 +74,7 @@ const image3 = ref(null)
       class="mx-auto"
       width="500"
     >
-      <v-form @submit.prevent>
+      <v-form @submit.prevent="submitForm">
         <TextInput
           v-model="name"
           :rules="requreRule"
