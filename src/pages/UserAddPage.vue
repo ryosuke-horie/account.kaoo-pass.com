e44eac800ce2046ba1c5d9e8e802e6cd7c13dc41
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-03-29 19:49:45 +0900

    inputのフィールドComponent作成

--- src/pages/UserAddPage.vue
+++ src/pages/UserAddPage.vue
@@ -1,8 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+import InputField from '../components/Form/TextInput.vue'
+
+// 氏名
+const name = ref('') // 氏名
+const nameRule = [
+  value => {
+    if (value) return true
+    return '必須項目です。'
+  },
+]
+
+
 </script>
 
 <template>
-  <div>
-    <h1>User Add Page</h1>
-  </div>
+  <v-sheet class="mx-auto" width="300">
+    <v-form @submit.prevent>
+      <InputField v-model="name" :rules="nameRule" label="氏名" />
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
