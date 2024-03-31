1cc0bbe9f19540ad43702322e65418b656fb0ef3
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-03-31 16:23:49 +0900

    アカウント作成後、リダイレクト

--- src/pages/StripeCreatePage.vue
+++ src/pages/StripeCreatePage.vue
@@ -1,7 +1,34 @@
 <script setup>
 import AppBar from '../components/util/AppBar.vue'
+import axios from 'axios';
+import { ref } from 'vue';
+
+const error = ref(null);
+
+const redirectToStripeToCreateAccount = async () => {
+    try {
+        const response = await axios.get('https://api.kaoo-pass.com/api/stripe/create', {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+
+        if (response.data.onboarding_url) {
+            window.location.href = response.data.onboarding_url;
+        } else {
+            error.value = 'リダイレクトURLが取得できませんでした';
+        }
+    } catch (err) {
+        console.error(err);
+        error.value = 'アカウント作成リクエストに失敗しました';
+    }
+};
 </script>
 
 <template>
-  <AppBar title="Stripe決済用アカウント作成" />
+    <AppBar title="Stripe決済用アカウント作成" />
+    <v-col cols="auto">
+        <v-btn size="x-large" @click="redirectToStripeToCreateAccount()">アカウントを作成する</v-btn>
+        <v-alert v-if="error" type="error">{{ error }}</v-alert>
+    </v-col>
 </template>
