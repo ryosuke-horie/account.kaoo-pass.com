1bfa78fbbe0148bf006f1008b067bfdd252ad880
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-06 14:58:33 +0900

    成型

--- src/router.ts
+++ src/router.ts
@@ -1,37 +1,72 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import useAuth from './fooks/useAuth';
-import LoginPage from './pages/LoginPage.vue'
-import TopPage from './pages/TopPage.vue'
-import UserAddPage from './pages/UserAddPage.vue'
-import UserDashBoardPage from './pages/UserDashBoardPage.vue'
-import UserDetailpage from './pages/UserDetailpage.vue'
-import UserEditPage from './pages/UserEditPage.vue'
-import TermsPage from './pages/TermsPage.vue'
-import PrivacyPolocy from './pages/PrivacyPolocy.vue'
-import ContactPage from './pages/ContactPage.vue'
-import StripeCreatePage from './pages/StripeCreatePage.vue'
-import StripeCreateProductPage from './pages/StripeCreateProductPage.vue'
-import StripeProductListPage from './pages/StripeProductListPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import useAuth from "./fooks/useAuth";
+import LoginPage from "./pages/LoginPage.vue";
+import TopPage from "./pages/TopPage.vue";
+import UserAddPage from "./pages/UserAddPage.vue";
+import UserDashBoardPage from "./pages/UserDashBoardPage.vue";
+import UserDetailpage from "./pages/UserDetailpage.vue";
+import UserEditPage from "./pages/UserEditPage.vue";
+import TermsPage from "./pages/TermsPage.vue";
+import PrivacyPolocy from "./pages/PrivacyPolocy.vue";
+import ContactPage from "./pages/ContactPage.vue";
+import StripeCreatePage from "./pages/StripeCreatePage.vue";
+import StripeCreateProductPage from "./pages/StripeCreateProductPage.vue";
+import StripeProductListPage from "./pages/StripeProductListPage.vue";
 
 const routes = [
-  { path: '/', name: 'Top', component: TopPage, meta: { requiresAuth: true } },
-  { path: '/login', name: 'Login', component: LoginPage },
-  { path: '/term', name: 'Term', component: TermsPage },
-  { path: '/contact', name: 'Contact', component: ContactPage },
-  { path: '/privacy', name: 'Privacy', component: PrivacyPolocy },
-  { path: '/dashboard', name: 'Dashboard', component: UserDashBoardPage, meta: { requiresAuth: true } },
-  { path: '/user/add', name: 'UserAdd', component: UserAddPage, meta: { requiresAuth: true } },
-  { path: '/user/:id', name: 'UserDetail', component: UserDetailpage, meta: { requiresAuth: true } },
-  { path: '/user/:id/edit', name: 'UserEdit', component: UserEditPage, meta: { requiresAuth: true } },
-  { path: '/stripe/create', name: 'Stripe', component: StripeCreatePage, meta: { requiresAuth: true } },
-  { path: '/stripe/create/products', name: 'StripeProduct', component: StripeCreateProductPage, meta: { requiresAuth: true } },
-  { path: '/stripe/products', name: 'StripeProductList', component: StripeProductListPage, meta: { requiresAuth: true } },
-]
+  { path: "/", name: "Top", component: TopPage, meta: { requiresAuth: true } },
+  { path: "/login", name: "Login", component: LoginPage },
+  { path: "/term", name: "Term", component: TermsPage },
+  { path: "/contact", name: "Contact", component: ContactPage },
+  { path: "/privacy", name: "Privacy", component: PrivacyPolocy },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: UserDashBoardPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/user/add",
+    name: "UserAdd",
+    component: UserAddPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/user/:id",
+    name: "UserDetail",
+    component: UserDetailpage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/user/:id/edit",
+    name: "UserEdit",
+    component: UserEditPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/stripe/create",
+    name: "Stripe",
+    component: StripeCreatePage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/stripe/create/products",
+    name: "StripeProduct",
+    component: StripeCreateProductPage,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/stripe/products",
+    name: "StripeProductList",
+    component: StripeProductListPage,
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
 // グローバルビフォーガード
 // ルート遷移前に実行され、未ログインならログインページにリダイレクト
@@ -42,7 +77,7 @@ router.beforeEach((to, _from, next) => {
     // ログイン状態をチェック
     if (!isAuthenticated.value) {
       // ログインしていない場合、ログインページにリダイレクト
-      next({ path: '/login', query: { redirect: to.fullPath } });
+      next({ path: "/login", query: { redirect: to.fullPath } });
     } else {
       next();
     }
@@ -50,6 +85,5 @@ router.beforeEach((to, _from, next) => {
     next();
   }
 });
-
 
 export default router;
